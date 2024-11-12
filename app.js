696af9adab19f0820477aa974c4c852a7f1bb9a0
calderon104 <gonza.rc1004@gmail.com>
2024-11-12 14:40:42 -0300

    agregado env/.vrmc

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import session from "express-session";
 import path from "path";
+import dotenv from "dotenv";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+import { dirname, join } from "path";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
@@ -10,13 +11,14 @@ import morgan from "morgan";
 import rPaciente from "./routes/rPacientes.js";
 import rSecretaria from "./routes/rSecretaria.js";
 import rMain from "./routes/rMain.js";
-import rAdmin from './routes/rAdmin.js';
+import rAdmin from "./routes/rAdmin.js";
 // import {isAuthenticated} from "./middlewares/auth.js";
-const app = express();
-const PORT = 3000;
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+dotenv.config({ path: join(__dirname, ".env") });
 
 app.use(cors());
 app.use(helmet());
@@ -27,25 +29,24 @@ app.set("view engine", "pug");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(rMain)
+app.use(rMain);
 app.use(rPaciente);
 app.use(rSecretaria);
 app.use(rAdmin);
 //app.use(isAuthenticated,rSecretaria)
 //app.use(error.e404);
 
-
 app.use((req, res, next) => {
-    res.status(404).render('error', { message: 'Página no encontrada' });
-  });
-  
-  // Manejo de errores 500
-  app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).render('error', { message: 'Error interno del servidor' });
-  });
+  res.status(404).render("error", { message: "Página no encontrada" });
+});
+
+// Manejo de errores 500
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render("error", { message: "Error interno del servidor" });
+});
 app.listen(PORT, () => {
-    console.log(`Servidor en http://localhost:${PORT}`);
-  });
+  console.log(`Servidor en http://localhost:${PORT}`);
+});
