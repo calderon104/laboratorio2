31a9fa2826d618c59292304d7438baed183b6d66
calderon104 <gonza.rc1004@gmail.com>
2024-11-13 11:11:39 -0300

    db

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 import mysql2 from "mysql2/promise";
 
 const connection  = mysql2.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_DATABASE || "agenda-consultorio2",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
 });
