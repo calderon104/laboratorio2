52a8c6f3c69645d1e910b8b44a489350a30ff19c
calderon104 <gonza.rc1004@gmail.com>
2024-11-13 11:19:38 -0300

    .

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
-//  import mysql2 from "mysql2/promise";
+ import mysql2 from "mysql2/promise";
 
- import mysql2 from "mysql2";
+//  import mysql2 from "mysql2";
 // const connection  = mysql2.createPool({
 //   host: process.env.DB_HOST,
 //   user: process.env.DB_USER ,
@@ -13,14 +13,10 @@
 // export default connection ;
 
 
-const connection  = mysql2.createConnection({
+const connection = new mysql2.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER ,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-});
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Conectado a la base de datos MySQL');
 });
 export default connection ;
