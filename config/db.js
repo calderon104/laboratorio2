443c300f27b35356e0f81323d907ff046e958993
calderon104 <gonza.rc1004@gmail.com>
2024-11-13 11:14:52 -0300

    db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,25 @@
-import mysql2 from "mysql2/promise";
+ import mysql2 from "mysql2/promise";
 
-const connection  = mysql2.createPool({
+// const connection  = mysql2.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER ,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+// });
+
+// export default connection ;
+
+
+const connection  = mysql2.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER ,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
 });
-
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Conectado a la base de datos MySQL');
+});
 export default connection ;
-
-
