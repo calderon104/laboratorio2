19d5a85b83032f57c3c905259e8d856a4eefdfa9
calderon104 <gonza.rc1004@gmail.com>
2024-11-13 21:14:27 -0300

    .

--- config/db.js
+++ config/db.js
@@ -1,27 +1,13 @@
-// import dotenv from "dotenv";
-// import mysql2 from "mysql2/promise";
-// dotenv.config();
-// const connection = mysql2.createConnection({
-//   host: process.env.MYSQL_HOST || "localhost",
-//   user: process.env.MYSQL_USER || "root",
-//   password: process.env.MYSQL_PASSWORD || "",
-//   database: process.env.MYSQL_DATABASE || "agenda-consultorio2",
-//   port: process.env.MYSQL_PORT || 3000,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-// });
-
-// export default connection;
-
 import mysql2 from "mysql2/promise";
 
-const connection  = mysql2.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_DATABASE || "agenda-consultorio2",
+const connection = mysql2.createPool({
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
+  port: process.env.MYSQL_ADDON_PORT || 3306, // Asegúrate de agregar el puerto también
   waitForConnections: true,
   connectionLimit: 10,
 });
 
-export default connection ;
+export default connection;
