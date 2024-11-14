d6cc2ae8baf12e2ebddee3e4d5610e2c86838b5d
calderon104 <gonza.rc1004@gmail.com>
2024-11-13 21:06:54 -0300

    .

--- config/db.js
+++ config/db.js
@@ -1,27 +1,27 @@
-import dotenv from "dotenv";
-import mysql2 from "mysql2/promise";
-dotenv.config();
-const connection = mysql2.createConnection({
-  host: process.env.MYSQL_HOST || "localhost",
-  user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "",
-  database: process.env.MYSQL_DATABASE || "agenda-consultorio2",
-  port: process.env.MYSQL_PORT || 3000,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
-
-export default connection;
-
+// import dotenv from "dotenv";
 // import mysql2 from "mysql2/promise";
-
-// const connection  = mysql2.createPool({
-//   host: process.env.DB_HOST || "localhost",
-//   user: process.env.DB_USER || "root",
-//   password: process.env.DB_PASSWORD || "",
-//   database: process.env.DB_DATABASE || "agenda-consultorio2",
+// dotenv.config();
+// const connection = mysql2.createConnection({
+//   host: process.env.MYSQL_HOST || "localhost",
+//   user: process.env.MYSQL_USER || "root",
+//   password: process.env.MYSQL_PASSWORD || "",
+//   database: process.env.MYSQL_DATABASE || "agenda-consultorio2",
+//   port: process.env.MYSQL_PORT || 3000,
 //   waitForConnections: true,
 //   connectionLimit: 10,
 // });
 
-// export default connection ;
+// export default connection;
+
+import mysql2 from "mysql2/promise";
+
+const connection  = mysql2.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_DATABASE || "agenda-consultorio2",
+  waitForConnections: true,
+  connectionLimit: 10,
+});
+
+export default connection ;
